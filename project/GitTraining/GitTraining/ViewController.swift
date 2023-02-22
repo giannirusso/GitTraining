cbc7a95eeaabd0f748891bd357395d0a14fd49c8
Djanibek Khudaybergenov <djanibek.k@gmail.com>
2023-02-22 23:27:58 +0300

    создал проект

---
+++ project/GitTraining/GitTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTraining
+//
+//  Created by D K on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
